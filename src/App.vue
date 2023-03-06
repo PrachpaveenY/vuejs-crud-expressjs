2a501bc83d5ad8ccf83987944083d37fc8fac14f
Prachpaveen Y <prachpaveen888@gmail.com>
2023-03-07 00:39:39 +0700

    update front-end back-end

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <div>
         <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
             <div class="container">
-                <router-link :to="{ path: '/' }" class="navbar-brand float-left">Main CRUD</router-link>
+                <router-link :to="{ path: '/' }" class="navbar-brand float-left">CRUD Project</router-link>
             <ul class="nav navbar-nav flex-row float-right">
                 <li class="nav-item">
                     <router-link to="/" class="nav-link pr-3">Create Student</router-link>
